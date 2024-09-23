4f73a7baf22a657d3877ace9adc1feb7a0d0d235
omrina <omri.nathanson@gmail.com>
2024-09-23 14:14:00 +0300

    1. fix error trying to register task if it already exists- just continuing
    2. make it 2 tasks for push and pull:
    - push triggers every 30 min and if the app is launched
    - pull triggers when user logs on and when app is closed.

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # auto-git-updates
 Make your git repo automatically up to date with powershell:
-* Choose an app (for example *notepad*).
-* When the app you chose is **launched** -> perform `git pull`.
-* When the app you chose is **closed** -> perform `git add & commit & push`.
-* It's all performed with Windows' **schedule tasks** for pull and for push, triggered by event log of process creation and process termination.
+## This tool creates 2 scheduled tasks:
+*First choose an app (for example *notepad*)
+1. When the app you chose is **launched** OR you **log in** to computer -> perform `git pull`
+2. When the app you chose is **closed** OR **every 30 minutes** -> perform `git add & commit & push`
+
+The app launch and close and the computer logon are triggers the task by event log of process creation, process termination and account logon.  
 
 > **_NOTE:_**  It's assumed your default branch is **main**.  
-> if you wish to change it, edit `git_pull.ps1` line 5 and change the branch there.
+> If you wish to change it, edit `git_pull.ps1` line 5 and change the branch.  
+  
+## Installation  
+* Open CMD as **admin**.  
+* Run `powershell schedule_git_updater.ps1`.  
+* Follow 2 simple input prompts (app path & repo path)  
 
-# Run
-* open CMD as **admin**.
-* run `powershell schedule_git_updater.ps1`.
-* follow the 3 simple input prompts (app path, repo path & app name)
-* Enjoy! :)
+# Enjoy! :)
--- schedule_git_updater.ps1
+++ schedule_git_updater.ps1
@@ -4,69 +4,34 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 Write-Output "Welcome to auto git updates!"
+Write-Output ""
 Write-Output "first we need to do some set up:"
-Write-Output ""
-Write-Output "enabling process creation logs..."
+Write-Output "enabling process CREATION logs..."
 auditpol /set /subcategory:"Process Creation" /success:enable /failure:enable
-Write-Output ""
-Write-Output "enabling process termination logs..."
+Write-Output "enabling process TERMINATION logs..."
 auditpol /set /subcategory:"Process Termination" /success:enable /failure:enable
+Write-Output "enabling process LOGON logs..."
+auditpol /set /subcategory:"Logon" /success:enable /failure:enable
+auditpol /set /category:"Account Logon" /success:enable /failure:enable
+auditpol /set /subcategory:"Special Logon" /success:enable /failure:enable
 
 Write-Output ""
-Write-Output "next step, give us some info:"
+Write-Output "final step, give us some info:"
 $app_path = Read-Host "enter the app exe full path"
 $git_path = Read-Host "enter git repo full path"
-$app_name = Read-Host "enter app name for task readability"
+$git_folder_name = ($git_path -split '\\')[-1]
+$app_name = [System.IO.Path]::GetFileNameWithoutExtension($app_path)
 $push_script_path = "$PSScriptRoot\git_push.ps1"
 $pull_script_path = "$PSScriptRoot\git_pull.ps1"
-$current_user = $Env:UserName
+$current_user = (Get-WmiObject -Class Win32_ComputerSystem).UserName
 
-$pullTaskXml = @"
+function SchedulePushOnAppCloseAndEvery30Min {
+  $pushTaskXml = @"
 <Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
   <RegistrationInfo>
     <Date>$(Get-Date -Format "yyyy-MM-ddTHH:mm:ss")</Date>
     <Author>$current_user</Author>
-    <Description>$app_name pull from git when app starts</Description>
-  </RegistrationInfo>
-  <Triggers>
-    <EventTrigger>
-      <Enabled>true</Enabled>
-      <Subscription>
-      &lt;QueryList&gt;&lt;Query Id="0" Path="Security"&gt;&lt;Select Path="Security"&gt;*[System[(EventID=4688)]]
-              and
-            *[EventData[Data[@Name='NewProcessName'] and (Data='$app_path')]]
-            &lt;/Select&gt;&lt;/Query&gt;&lt;/QueryList&gt;</Subscription>
-    </EventTrigger>
-  </Triggers>
-  <Principals>
-    <Principal id="Author">
-      <UserId>$current_user</UserId>
-      <LogonType>S4U</LogonType>
-      <RunLevel>HighestAvailable</RunLevel>
-    </Principal>
-  </Principals>
-  <Settings>
-    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
-    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
-    <StopIfGoingOnBatteries>false</StopIfGoingOnBatteries>
-    <StartWhenAvailable>true</StartWhenAvailable>
-    <AllowHardTerminate>true</AllowHardTerminate>
-  </Settings>
-  <Actions Context="Author">
-    <Exec>
-      <Command>powershell.exe</Command>
-      <Arguments>-File `"$pull_script_path`" $git_path $app_name</Arguments>
-    </Exec>
-  </Actions>
-</Task>
-"@
-
-$pushTaskXml = @"
-<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
-  <RegistrationInfo>
-    <Date>$(Get-Date -Format "yyyy-MM-ddTHH:mm:ss")</Date>
-    <Author>$current_user</Author>
-    <Description>$app_name push to git when app terminates</Description>
+    <Description>$app_name push to git when app terminates and every 30 minutes</Description>
   </RegistrationInfo>
   <Triggers>
     <EventTrigger>
@@ -77,6 +42,13 @@ $pushTaskXml = @"
             *[EventData[Data[@Name='ProcessName'] and (Data='$app_path')]]
             &lt;/Select&gt;&lt;/Query&gt;&lt;/QueryList&gt;</Subscription>
     </EventTrigger>
+    <TimeTrigger>
+        <Enabled>true</Enabled>
+        <StartBoundary>$(Get-Date -Format "yyyy-MM-ddTHH:mm:ss")</StartBoundary>
+        <Repetition>
+            <Interval>PT30M</Interval>
+        </Repetition>
+    </TimeTrigger>
   </Triggers>
   <Principals>
     <Principal id="Author">
@@ -101,8 +73,80 @@ $pushTaskXml = @"
 </Task>
 "@
 
-Register-ScheduledTask -TaskName "$app_name push task" -Xml $pushTaskXml
-Write-Output "Scheduled task of PUSH created successfully."
-Register-ScheduledTask -TaskName "$app_name pull task" -Xml $pullTaskXml
-Write-Output "Scheduled task of PULL created successfully."
+  if (-not $(Get-ScheduledTask -TaskName "$git_folder_name git push by $app_name" -ErrorAction SilentlyContinue)) {
+    Register-ScheduledTask -TaskName "$git_folder_name git push by $app_name" -Xml $pushTaskXml
+    Write-Output "Scheduled task of 'PUSH' created successfully."
+  }
+  else {
+    Write-Output "Scheduled task of 'PUSH' already exists, continuing..."
+  }
+}
+
+function SchedulePullOnAppOpenAndOnLogon {
+  $pullTaskXml = @"
+<Task version="1.2" xmlns="http://schemas.microsoft.com/windows/2004/02/mit/task">
+  <RegistrationInfo>
+    <Date>$(Get-Date -Format "yyyy-MM-ddTHH:mm:ss")</Date>
+    <Author>$current_user</Author>
+    <Description>$app_name pull from git when app starts and on user logon</Description>
+  </RegistrationInfo>
+  <Triggers>
+    <EventTrigger>
+      <Enabled>true</Enabled>
+      <Subscription>
+      &lt;QueryList&gt;&lt;Query Id="0" Path="Security"&gt;&lt;Select Path="Security"&gt;*[System[(EventID=4688)]]
+              and
+            *[EventData[Data[@Name='NewProcessName'] and (Data='$app_path')]]
+            &lt;/Select&gt;&lt;/Query&gt;&lt;/QueryList&gt;</Subscription>
+    </EventTrigger>
+    <EventTrigger>
+    <Enabled>true</Enabled>
+    <Subscription>
+      &lt;QueryList&gt;
+        &lt;Query Id="0" Path="Security"&gt;
+          &lt;Select Path="Security"&gt;
+              *[System[(EventID=4624)]]
+              and
+              *[EventData[Data[@Name='LogonType'] and (Data='2')]]
+            &lt;/Select&gt;
+          &lt;/Query&gt;
+      &lt;/QueryList&gt;
+    </Subscription>
+  </EventTrigger>
+  </Triggers>
+  <Principals>
+    <Principal id="Author">
+      <UserId>$current_user</UserId>
+      <LogonType>S4U</LogonType>
+      <RunLevel>HighestAvailable</RunLevel>
+    </Principal>
+  </Principals>
+  <Settings>
+    <MultipleInstancesPolicy>IgnoreNew</MultipleInstancesPolicy>
+    <DisallowStartIfOnBatteries>false</DisallowStartIfOnBatteries>
+    <StopIfGoingOnBatteries>false</StopIfGoingOnBatteries>
+    <StartWhenAvailable>true</StartWhenAvailable>
+    <AllowHardTerminate>true</AllowHardTerminate>
+  </Settings>
+  <Actions Context="Author">
+    <Exec>
+      <Command>powershell.exe</Command>
+      <Arguments>-File `"$pull_script_path`" $git_path $app_name</Arguments>
+    </Exec>
+  </Actions>
+</Task>
+"@
+
+  if (-not $(Get-ScheduledTask -TaskName "$git_folder_name git pull by $app_name" -ErrorAction SilentlyContinue)) {
+    Register-ScheduledTask -TaskName "$git_folder_name git pull by $app_name" -Xml $pullTaskXml
+    Write-Output "Scheduled task of 'PULL' created successfully."
+  }
+  else {
+    Write-Output "Scheduled task of 'PULL' already exists, continuing..."
+  }
+}
+
+SchedulePushOnAppCloseAndEvery30Min
+SchedulePullOnAppOpenAndOnLogon
+
 Write-Output "All done! enjoy :-)"
